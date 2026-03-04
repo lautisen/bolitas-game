bddd2c421dd2fe608994f15ff24e965d1d81a353
lautisen <bocalauti@gmail.com>
2026-03-04 01:50:08 +0100

    Fix URL github pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // Setting base to './' allows the built project to work on subpaths like GitHub Pages
-    base: './',
+    // Setting base allows the built project to work on GitHub Pages subfolder
+    base: '/bolitas-game/',
 });
